788b516d04ab76acc52491c0c7749a50911f9f55
Jacob Medina <jacobmedina1618@gmail.com>
2023-09-28 22:41:04 -0400

    configured manifest.json build

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,24 @@
 const butInstall = document.getElementById('buttonInstall');
+let installEvent = null;
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    installEvent = event;
+    butInstall.classList.remove('invisible');
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (!installEvent) return;
+    
+    installEvent.prompt();
+    installEvent = null;
+    butInstall.classList.add('invisible');
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    installEvent = null;
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
@@ -13,25 +14,50 @@ module.exports = () => {
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
+      clean: true
     },
     plugins: [
-      new HtmlWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+        filename: 'index.html'
+      }),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'sw.js'
       }),
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: 'Just Another Text Editor',
         short_name: 'JATE',
         description: 'Clean and simple text editor.',
         background_color: '#ffffff',
-        crossorigin: null, //can be null, use-credentials or anonymous
+        theme_color: '#ffffff',
+        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+            sizes: [96, 144],
+            destination: path.join('assets', 'icons'),
           },
+          {
+            src: path.resolve('src/images/logo.png'),
+            size: 512, 
+            destination: path.join('assets', 'icons'),
+            purpose: 'maskable',
+          },
+          // {
+          //   src: path.resolve('favicon.ico'),
+          //   size: 48
+          // }
         ]
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: "favicon.ico", to: "favicon.ico" },
+        ],
       }),
     ],
     module: {
